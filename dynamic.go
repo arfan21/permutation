c291f6979ce44d8f5b3519183a96f6e28bf8a0ac
Muhamad Elang Ramadhan <58929520+mugeki@users.noreply.github.com>
2021-05-26 23:08:12 +0700

    Menambah komentar

--- dynamic.go
+++ dynamic.go
@@ -5,6 +5,7 @@ import (
 )
 
 func Dynamic(n, r int) int {
+	// Pendefinisian array 2 dimensi untuk menyimpan nilai hasil permutasi 
 	P := make([][]int, n+2)
 
 	for i := range P {
@@ -12,16 +13,21 @@ func Dynamic(n, r int) int {
 		P[i] = make([]int, r+2)
 	}
 
-	for i := 0; i <= n; i++ {
-		for j := 0; j <= int(math.Min(float64(i), float64(r))); j++ {
-			if j == 0 {
+	// Looping untuk array dimensi pertama, dilakukan sebanyak n+1 kali
+	for i := 0; i <= n; i++ {	
+		// Looping untuk array dimensi kedua
+		// Untuk setiap j, dilakukan looping sebanyak nilai minimum antara i dan r
+		// Hal ini karena untuk setiap j > i maka hasil permutasi akan selalu 0
+		for j := 0; j <= int(math.Min(float64(i), float64(r))); j++ {	
+			if j == 0 {			// Basis ketika P(i,0)
 				P[i][j] = 1
-			} else {
+			} else {			// Menghitung hasil P(i,j) menggunakan nilai yang telah disimpan pada array
 				P[i][j] = P[i-1][j] + (j * P[i-1][j-1])
 			}
-			P[i][j+1] = 0
+			// Untuk setiap j > i maka hasil permutasi akan selalu 0
+			P[i][j+1] = 0 
 		}
 	}
-
+	// Mengembalikan hasil P(n,r)
 	return P[n][r]
 }
