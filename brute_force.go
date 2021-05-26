82b062abbe78a22665451066701b4874eb580a5d
Muhamad Elang Ramadhan <58929520+mugeki@users.noreply.github.com>
2021-05-26 23:09:55 +0700

    Menambah komentar

--- brute_force.go
+++ brute_force.go
@@ -1,9 +1,9 @@
 package permutation
 
 func BruteForce(n, r int) int {
-	P := 1
-	for i := 0; i < r; i++ {
-		P *= (n - i)
+	P := 1		// Pendefinisian variabel yang menampung hasil permutasi
+	for i := 0; i < r; i++ {	// Looping sebanyak r kali 
+		P *= (n - i)	// Operasi perkalian antara P dengan nilai (n-i)
 	}
-	return P
+	return P	// Mengembalikan hasil permutasi
 }
